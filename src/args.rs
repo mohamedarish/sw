80089ba9da01e4c1372a9af4226666e4a0205b7e
Mohamed Arish T P <arish1248@gmail.com>
2023-08-15 16:56:45 +0530

    Added info to cli arguents and also fixed a typo found using the typo command

--- src/args.rs
+++ src/args.rs
@@ -5,11 +5,14 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
+    /// Path to list (Default = ".")
     pub path: Option<PathBuf>,
 
+    /// Display all files including hidden files
     #[arg(short, long)]
     pub all: bool,
 
+    /// Display the output in list format
     #[arg(short, long)]
     pub list: bool,
 }
